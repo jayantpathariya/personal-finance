8f692bf7ac75056f02b0f5dcc354b0d2b0f552b9
Jayant Pathariya <jayantmehra02@gmail.com>
2024-11-29 21:11:27 +0530

    Input component with label support

--- src/app/(auth)/_components/sign-in-form.tsx
+++ src/app/(auth)/_components/sign-in-form.tsx
@@ -3,9 +3,13 @@ import { Input } from "@/components/ui/input";
 export const SignInForm = () => {
   return (
     <form className="bg-white">
-      <Input placeholder="Email" />
-      <Input placeholder="Password" error="Password is required." />
-      <Input placeholder="Search..." search />
+      <Input placeholder="Email" label="Email" />
+      <Input
+        placeholder="Password"
+        label="Password"
+        error="Password is required."
+      />
+      <Input placeholder="Search..." label="Search" search />
       <Input placeholder="Placeholder" prefixI />
     </form>
   );
--- src/components/ui/input.tsx
+++ src/components/ui/input.tsx
@@ -6,24 +6,44 @@ type InputProps = React.InputHTMLAttributes<HTMLInputElement> & {
   error?: string;
   search?: boolean;
   prefixI?: boolean;
+  label?: string;
 };
 
-export const Input = ({ error, search, prefixI, ...props }: InputProps) => {
+export const Input = ({
+  error,
+  search,
+  prefixI,
+  label,
+  ...props
+}: InputProps) => {
   return (
-    <div className="relative">
-      {prefixI && (
-        <span className="absolute left-3 top-2.5 text-beige-500">$</span>
+    <div className="flex flex-col">
+      {label && (
+        <label
+          htmlFor={props.id}
+          className="mb-1 text-xs font-bold text-gray-500"
+        >
+          {label}
+        </label>
       )}
-      <input
-        className={cn(
-          "rounded-lg border border-beige-500 px-5 py-2.5 text-sm text-beige-500 outline-none",
-          search && "pr-8",
-          error && "border-red",
-          prefixI && "pl-8",
+      <div className="relative">
+        {prefixI && (
+          <span className="absolute left-3 top-2.5 text-beige-500">$</span>
         )}
-        {...props}
-      />
-      {search && <Search className="absolute right-3 top-3.5 text-gray-900" />}
+
+        <input
+          className={cn(
+            "rounded-lg border border-beige-500 px-5 py-2.5 text-sm text-beige-500 outline-none",
+            search && "pr-8",
+            error && "border-red",
+            prefixI && "pl-8",
+          )}
+          {...props}
+        />
+        {search && (
+          <Search className="absolute right-3 top-3.5 text-gray-900" />
+        )}
+      </div>
       {error && (
         <p className="text-right">
           <small className="text-red">{error}</small>
